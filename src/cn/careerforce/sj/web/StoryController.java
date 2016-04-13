a05c5a85b610c0142593a1d2343a2936bd72da0c
nanmeiying <529726271@qq.com>
2016-04-13 15:53:40 +0800

    首次提交

--- src/cn/careerforce/sj/web/StoryController.java
+++ src/cn/careerforce/sj/web/StoryController.java
@@ -107,7 +107,7 @@ public class StoryController {
             String description = story.get("description").toString();
             if (!"".equals(description)) {
                 String[] des = description.split("\\[\\$\\*\\$\\]");
-                StringBuffer sb = new StringBuffer("<html><head><style type='text/css'>p{text-indent:2em;color: #333;line-height: 24px;}div {width:100%;}img {display:block; margin:10px auto;max-width:100%;}</style></head><body id='body'>");
+                StringBuffer sb = new StringBuffer("<html><head><style type='text/css'>body{padding:0;margin:0;}p{margin-bottom:10px;color:#585858;text-align:left;font-size:12px;line-height:24px;}img{width:100%;margin-top:6px;}</style></head><body id='body'>");
                 for (int i = 0; i < des.length; i++) {
                     if (des[i].startsWith("http")) {
                         sb.append("<div><img src='").append(des[i]).append("'/></div>");
